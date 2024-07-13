7881bb7ef1a053a43c17a46c19eadf46682ec0da
Dinesh10731j <dinesh1122@gmail.com>
2024-07-13 16:05:29 +0545

    update done

--- controller/updatepersonalinfo.controller.js
+++ controller/updatepersonalinfo.controller.js
@@ -2,16 +2,19 @@ const ScheduleModel = require("../model/scheduledonation.model");
 
 const UpdatePersonalInfo = async (req, res) => {
   try {
+    const { userid } = req.params;
 
-    const {userid} = req.params;
+    const { email, phoneNumber, fullName } = req.body;
+    console.log(email, phoneNumber, fullName);
 
-    const {email,phoneNumber,fullName}= req.body;
-    console.log(email,phoneNumber,fullName);
-  
-    console.log('This is user id ',userid);
+    console.log("This is user id ", userid);
 
-    const updatedInfo = await ScheduleModel.findByIdAndUpdate(userid,{email,phoneNumber,fullName});
-    console.log(updatedInfo)
+    const updatedInfo = await ScheduleModel.findByIdAndUpdate(
+      userid,
+      { email, phoneNumber, fullName },
+      { new: true, runValidators: true }
+    );
+    console.log(updatedInfo);
 
     if (!updatedInfo) {
       return res.status(404).send({ msg: "User not found", success: false });
@@ -20,8 +23,10 @@ const UpdatePersonalInfo = async (req, res) => {
     return res.status(200).send({ data: updatedInfo, success: true });
   } catch (err) {
     console.error(err); // Log the error for debugging
-    return res.status(500).send({ msg: "Internal server error", success: false });
+    return res
+      .status(500)
+      .send({ msg: "Internal server error", success: false });
   }
 };
 
-module.exports = {UpdatePersonalInfo};
+module.exports = { UpdatePersonalInfo };
