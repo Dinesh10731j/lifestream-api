333b00297025f7c13cb950d380885e289b31ea62
Dinesh10731j <dinesh1122@gmail.com>
2024-07-13 18:34:33 +0545

    updateMany  is used to update all the documents

--- controller/updatepersonalinfo.controller.js
+++ controller/updatepersonalinfo.controller.js
@@ -2,27 +2,21 @@ const ScheduleModel = require("../model/scheduledonation.model");
 
 const UpdatePersonalInfo = async (req, res) => {
   try {
-    const { useremail} = req.params; // The current email to find the user
-    const { email, phoneNumber, fullName } = req.body; // The new email and other fields to update
+    const { useremail} = req.params; 
+    const { email, phoneNumber, fullName } = req.body; 
 
-    console.log('Current Email:', email);
-    console.log('New Email:', email, 'New Phone Number:', phoneNumber, 'New Full Name:', fullName);
-
-    const updatedInfo = await ScheduleModel.findOneAndUpdate(
+    const updatedInfo = await ScheduleModel.updateMany(
       { useremail }, // Find the user by current email
       { email: email, phoneNumber, fullName }, // Update to new email and other fields
       { new: true, runValidators: true } // Options: return updated document, run validators
     );
-
-    console.log('Updated Info:', updatedInfo);
-
     if (!updatedInfo) {
       return res.status(404).send({ msg: "User not found", success: false });
     }
 
     return res.status(200).send({ data: updatedInfo, success: true });
   } catch (err) {
-    console.error(err); // Log the error for debugging
+    
     return res.status(500).send({ msg: "Internal server error", success: false });
   }
 };
