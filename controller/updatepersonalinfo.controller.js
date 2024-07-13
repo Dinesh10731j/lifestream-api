03efae623b30fff13e78d8b6170ba6dd5a488f86
Dinesh10731j <dinesh1122@gmail.com>
2024-07-13 16:21:41 +0545

    done

--- controller/updatepersonalinfo.controller.js
+++ controller/updatepersonalinfo.controller.js
@@ -2,14 +2,14 @@ const ScheduleModel = require("../model/scheduledonation.model");
 
 const UpdatePersonalInfo = async (req, res) => {
   try {
-    const { email } = req.params; // The current email to find the user
-    const { newEmail, phoneNumber, fullName } = req.body; // The new email and other fields to update
+    const { useremail} = req.params; // The current email to find the user
+    const { email, phoneNumber, fullName } = req.body; // The new email and other fields to update
 
     console.log('Current Email:', email);
     console.log('New Email:', newEmail, 'New Phone Number:', phoneNumber, 'New Full Name:', fullName);
 
     const updatedInfo = await ScheduleModel.findOneAndUpdate(
-      { email }, // Find the user by current email
+      { useremail }, // Find the user by current email
       { email: newEmail, phoneNumber, fullName }, // Update to new email and other fields
       { new: true, runValidators: true } // Options: return updated document, run validators
     );
