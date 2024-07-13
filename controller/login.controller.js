43c727c7a5c1736f11d322360730cab58dfb615d
Dinesh10731j <dinesh1122@gmail.com>
2024-07-13 11:25:43 +0545

    did some adjustment in login

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-
+const UserSignupModel = require("../model/signup.model")
 const UserLogin = async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -16,11 +16,9 @@ const UserLogin = async (req, res) => {
             return res.status(400).send({ msg: 'Invalid credentials', success: false });
         }
 
-        // Generate token
         const token = await user.generateToken();
         user.token = token;
 
-        // Sending the response
         return res.status(200).send({ msg: "Login Successful", success: true, data: user });
     } catch (error) {
         return res.status(500).send({ msg: "Internal server error" });
