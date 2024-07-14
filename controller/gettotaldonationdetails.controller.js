783ff3ecdcc2ee9e7a85a9ce0fe13088ef610043
Dinesh10731j <dinesh1122@gmail.com>
2024-07-14 16:44:00 +0545

    changes again

--- controller/gettotaldonationdetails.controller.js
+++ controller/gettotaldonationdetails.controller.js
@@ -4,24 +4,23 @@ const GetTotalDonationdetails = async (req, res) => {
     try {
         let totaldonation = 0;
 
-        // Fetch all donation data
         const totaldonordonation = await ScheduleModel.find({});
 
-        // Iterate through each donation and check status
+        
         totaldonordonation.forEach(donation => {
             if (donation.status === 'Completed') {
                 totaldonation++;
+            }else{
+                totaldonation = 0;
             }
         });
 
-        // Add the total donation count to each donation object
         totaldonordonation.forEach(donation => {
             donation._doc.total = totaldonation; // _doc is used to directly modify the document's data
         });
 
         console.log("Total completed donations:", totaldonation);
 
-        // Send response with total donation count and all donation data
         res.status(200).send({ data: totaldonordonation, success: true });
 
     } catch (err) {
