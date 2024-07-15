3615bf7f4ea41a2bd9ceb811a3e95f0e4c517607
Dinesh10731j <dinesh1122@gmail.com>
2024-07-15 12:11:12 +0545

    chnages in requestblood controller

--- controller/requestblood.controller.js
+++ controller/requestblood.controller.js
@@ -1,9 +1,13 @@
 
 
 const RequestModel = require("../model/requestblood.model");
-const RequestBlood = (req,res)=>{
+const RequestBlood = async (req,res)=>{
     try{
         const {fullName,urgency,quantity,message,email,bloodGroup} = req.body;
+const RequestUsersdetails = await RequestModel.create({fullName,urgency,quantity,message,email,bloodGroup});
+
+res.status(201).send({msg:'Blood request send successfully!',data:RequestUsersdetails})
+
        
 
     }catch(err){
