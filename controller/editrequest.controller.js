50b590ce24c291c5c81887a804121823636cf455
Dinesh10731j <dinesh1122@gmail.com>
2024-07-16 16:54:56 +0545

    error fixed

--- controller/editrequest.controller.js
+++ controller/editrequest.controller.js
@@ -1,16 +1,23 @@
 const RequestModel = require("../model/requestblood.model");
-const EditRequest =async (req,res)=>{
-    try{
-        const {editid} = req.params;
 
-        const editedData = await RequestModel.findByIdAndUpdate(editid)
+const EditRequest = async (req, res) => {
+    try {
+        const { editid } = req.params;
+        const editedData = req.body;
 
-    }catch(err){
+        const updatedRequest = await RequestModel.findByIdAndUpdate(editid, editedData, {
+            new: true, // Returning  the updated document
+            runValidators: true 
+        });
 
+        if (!updatedRequest) {
+            return res.status(404).json({ msg: 'Request not found', status: false });
+        }
+
+        res.json({ message: 'Request updated successfully', data: updatedRequest, status: true });
+    } catch (err) {
+        res.status(500).send({ msg: 'Internal server error', error: err, status: false });
     }
-    
 }
-
-
 
 module.exports = EditRequest;
