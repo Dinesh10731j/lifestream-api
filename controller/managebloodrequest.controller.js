9b392f58b339baf0b9ab45b408ab8232732d4664
Dinesh10731j <dinesh1122@gmail.com>
2024-07-16 12:11:08 +0545

    i bug find and push

--- controller/managebloodrequest.controller.js
+++ controller/managebloodrequest.controller.js
@@ -5,7 +5,7 @@ const ManageBloodRequest = async (req,res)=>{
         const Requestblooddata = await RequestModel.find({});
         console.log('This is the  data of requestblood data',Requestblooddata)
 
-   if(Requestblooddata.length>0){
+   if(Requestblooddata.length>=0){
     return res.status(404).send({msg:'Blood request not found',success:false});
    }
 
