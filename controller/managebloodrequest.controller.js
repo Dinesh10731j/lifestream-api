d4e3fdde5ddeaa8a251a95293ec90902039e8c16
Dinesh10731j <dinesh1122@gmail.com>
2024-07-16 12:18:49 +0545

    data is send to fronetend beacause it was mission in earlier response

--- controller/managebloodrequest.controller.js
+++ controller/managebloodrequest.controller.js
@@ -9,7 +9,7 @@ const ManageBloodRequest = async (req,res)=>{
     return res.status(404).send({msg:'Blood request not found',success:false});
    }
 
-res.status(200).send({msg:"Receiptant Request fetch successfully",success:true})
+res.status(200).send({msg:"Receiptant Request fetch successfully",success:true,data:Requestblooddata})
 
     }catch(err){
         res.status(500).send({msg:'Internal server error',success:false},err)
