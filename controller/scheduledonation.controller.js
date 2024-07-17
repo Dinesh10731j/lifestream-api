efb7683fe79e35d03971a548bf3ec461a407dba9
Dinesh10731j <dinesh1122@gmail.com>
2024-07-17 18:43:52 +0545

    email changed and  did some changes in the code

--- controller/scheduledonation.controller.js
+++ controller/scheduledonation.controller.js
@@ -3,63 +3,60 @@ const ScheduleModel = require("../model/scheduledonation.model");
 const sendEmail = require("../services/nodemailer");
 
 const ScheduleDonation = async (req, res) => {
-    try {
-        const {
-            fullName,
-            email,
-            phoneNumber,
-            donationType,
-            date, 
-            time,
-            locationType,
-            address,
-            recentIllness,
-            medication,
-            chronicDiseases,
-            previousDonationDate,
-            notes,
-            recentTravel,
-            bloodQuantity,
-            bloodGroup
+  try {
+    const {
+      fullName,
+      email,
+      phoneNumber,
+      donationType,
+      date,
+      time,
+      locationType,
+      address,
+      recentIllness,
+      medication,
+      chronicDiseases,
+      previousDonationDate,
+      notes,
+      recentTravel,
+      bloodQuantity,
+      bloodGroup,
+    } = req.body;
 
-        } = req.body;
+    sendEmail(
+      email,
+      "Blood Donation Scheduled",
+      `Dear user ${fullName} you has scheduled a blood donation on ${date}. Please check your dashboard for details.https://lifeestream.netlify.app`
+    );
 
+    const donorSchedule = await ScheduleModel.create({
+      fullName,
+      email,
+      phoneNumber,
+      donationType,
+      date,
+      time,
+      locationType,
+      address,
+      recentIllness,
+      medication,
+      chronicDiseases,
+      previousDonationDate,
+      notes,
+      recentTravel,
+      bloodQuantity,
+      bloodGroup,
+    });
 
-        sendEmail(email, 'Blood Donation Scheduled', `Dear user ${fullName} you has scheduled a blood donation on ${date}. Please check your dashboard for details.https://lifeestream.netlify.app`);
-
-  
-
-        
-        const donorSchedule = await ScheduleModel.create({
-            fullName,
-            email,
-            phoneNumber,
-            donationType,
-            date,
-            time,
-            locationType,
-            address,
-            recentIllness,
-            medication,
-            chronicDiseases,
-            previousDonationDate,
-            notes,
-            recentTravel,
-            bloodQuantity,
-            bloodGroup
-        });
-
-       
-
-        res.status(201).send({
-            msg: "Schedule created Successfully",
-            data: donorSchedule,
-            success: true
-        });
-    } catch (error) {
-        console.error("Error:", error); // Log the error for debugging
-        res.status(500).send({ msg: "Internal server error", success: false });
-    }
+    res.status(201).send({
+      msg: "Schedule created Successfully",
+      data: donorSchedule,
+      success: true,
+    });
+  } catch (error) {
+    console.error("Error:", error); // Log the error for debugging
+    res.status(500).send({ msg: "Internal server error", success: false });
+  }
 };
 
 module.exports = ScheduleDonation;
