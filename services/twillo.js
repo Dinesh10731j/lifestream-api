45ce568cbbe1797d5fe726eba6964e7349e33020
Dinesh10731j <dinesh1122@gmail.com>
2024-07-17 14:56:47 +0545

    twillo error fixed

--- services/twillo.js
+++ services/twillo.js
@@ -5,20 +5,21 @@ const client = twilio(process.env.accountSid, process.env.authToken);
 
 const Twillo = (userMobileNumber) => {
   const formattedNumber = `+977${userMobileNumber.replace(/^0+/, '')}`;
-  console.log(formattedNumber)
-  
-  client.messages
+  console.log(formattedNumber);
+
+client.messages
     .create({
-      from: '+15856201636',
-      to: formattedNumber,
-      body: 'A donor has scheduled a blood donation. Please check your dashboard for details.'
+        body: 'A donor has scheduled a blood donation. Please check your dashboard for details.',
+        from: '+15856201636',
+
+              to: formattedNumber
     })
-    .then(message => {
-      console.log('Message sent with SID:', message.sid);
-    })
+    .then(message => console.log(message.sid))
     .catch(error => {
-      console.error('Error sending message:', error);
-    });
-};
+          console.error('Error sending message:', error);
+      });
+
+    }
+
 
 module.exports = Twillo;
