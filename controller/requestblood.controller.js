075da9c7a7d76d629fd6af753364e465965c94ad
Dinesh10731j <dinesh1122@gmail.com>
2024-07-18 10:28:38 +0545

    error fixed

--- controller/requestblood.controller.js
+++ controller/requestblood.controller.js
@@ -1,30 +1,29 @@
+const RequestModel = require("../model/requestblood.model");
+const RequestBlood = async (req, res) => {
+  try {
+    const { fullName, urgency, quantity, message, email, bloodGroup } =
+      req.body;
+    const RequestUsersdetails = await RequestModel.create({
+      fullName,
+      urgency,
+      quantity,
+      message,
+      email,
+      bloodGroup,
+    });
 
-const RequestBlood = async (req,res)=>{
-    try{
-        const {fullName,urgency,quantity,message,email,bloodGroup} = req.body;
-
-
- 
-
-
-const RequestUsersdetails = await RequestModel.create({fullName,urgency,quantity,message,email,bloodGroup});
-
-
-
-
-if(RequestUsersdetails){
-
-    return res.status(201).send({msg:'Blood request send successfully!',success:true})
-}
-
-
-return res.status(404).send({msg:'Failed to blood request',success:false})
-
-
-    }catch(err){
-res.status(500).json({msg:"Internal server error",success:false,err});
+    if (RequestUsersdetails) {
+      return res
+        .status(201)
+        .send({ msg: "Blood request send successfully!", success: true });
     }
-}
 
+    return res
+      .status(404)
+      .send({ msg: "Failed to blood request", success: false });
+  } catch (err) {
+    res.status(500).json({ msg: "Internal server error", success: false, err });
+  }
+};
 
 module.exports = RequestBlood;
