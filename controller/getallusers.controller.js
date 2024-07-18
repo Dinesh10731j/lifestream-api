817b17aaebd08393f5ac8d1568039251f58c1c5e
Dinesh10731j <dinesh1122@gmail.com>
2024-07-18 15:42:53 +0545

    error fixed

--- controller/getallusers.controller.js
+++ controller/getallusers.controller.js
@@ -1,23 +1,21 @@
 const UserSignupModel = require("../model/signup.model");
-const GetAlluser = async()=>{
-    try{
-        const LifestreamUser = await UserSignupModel.find({});
+const GetAlluser = async (req, res) => {
+  try {
+    const LifestreamUser = await UserSignupModel.find({});
 
+    if (LifestreamUser.length === 0) {
+      return res.status(400).send({ msg: "User not founs !", success: false });
+    }
 
-        if(LifestreamUser.length===0){
-            return res.status(400).send({msg:'User not founs !',success:false})
-        }
-
-
-        return res.status(200).send({msg:'LifestreamUsers fetch successfully',data:LifestreamUser});
-     
-    }  catch(err){
-
-
-        res.status(500).send({msg:'Internal server error '})
-        
-    } 
-}
-
+    return res
+      .status(200)
+      .send({
+        msg: "LifestreamUsers fetch successfully",
+        data: LifestreamUser,
+      });
+  } catch (err) {
+    res.status(500).send({ msg: "Internal server error " },err);
+  }
+};
 
 module.exports = GetAlluser;
