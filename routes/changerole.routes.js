08af57c566f2bffca8b629a7c9bff02b0e97e027
Dinesh10731j <dinesh1122@gmail.com>
2024-07-18 20:32:55 +0545

    id added in changerole

--- routes/changerole.routes.js
+++ routes/changerole.routes.js
@@ -1,6 +1,6 @@
 const ChangeRole = require("../controller/changerole.controller");
 const express = require("express");
 const UserRoleChangeRoute = express.Router();
-UserRoleChangeRoute.put("/changerole",ChangeRole);
+UserRoleChangeRoute.put("/changerole/:id",ChangeRole);
 
 module.exports = UserRoleChangeRoute;
