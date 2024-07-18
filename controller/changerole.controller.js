92d7a8e324d2b05091c721266d6b53dc431801d8
Dinesh10731j <dinesh1122@gmail.com>
2024-07-18 20:39:50 +0545

    change role error fixed

--- controller/changerole.controller.js
+++ controller/changerole.controller.js
@@ -1,22 +1,24 @@
 const UserSignupModel = require("../model/signup.model");
-const ChangeRole = async (req,res)=>{
-    try{
-        const {role} = req.body;
-    console.log('This is params of changeroles',req.params);
 
-const updatedRole = await UserSignupModel.findOneAndUpdate({role:role},{new:true});
+const ChangeRole = async (req, res) => {
+  try {
+    const { role } = req.body;
+    const { id } = req.params;
 
-if(!updatedRole){
-    return res.status(404).send({msg:'User not found',success:false})
-}
+    const updatedRole = await UserSignupModel.findOneAndUpdate(
+      { _id: id },
+      { role: role },
+      { new: true }
+    );
 
-return res.status(200).send({msg:'Role updated successfully',data:updatedRole})
-
-    }catch(err){
-        res.status(500).send({msg:'Internal server error',success:false,err:err});
+    if (!updatedRole) {
+      return res.status(404).send({ msg: 'User not found', success: false });
     }
 
-}
-
+    return res.status(200).send({ msg: 'Role updated successfully', data: updatedRole });
+  } catch (err) {
+    res.status(500).send({ msg: 'Internal server error', success: false, err: err });
+  }
+};
 
 module.exports = ChangeRole;
