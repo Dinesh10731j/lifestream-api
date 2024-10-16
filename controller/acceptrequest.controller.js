4e763d563f200d1312ea62d52dcca542ebe1fb24
Dinesh10731j <dinesh1122@gmail.com>
2024-10-16 16:23:04 +0545

    minor changes in acceptrequest controller

--- controller/acceptrequest.controller.js
+++ controller/acceptrequest.controller.js
@@ -1,10 +1,16 @@
+const mongoose = require("mongoose");
 const RequestModel = require("../model/requestblood.model");
+
 const acceptRequest = async (req, res) => {
   try {
     const { acceptId } = req.params;
 
     if (!acceptId) {
       return res.status(400).json({ msg: "acceptId is missing" });
+    }
+
+    if (!mongoose.isValidObjectId(acceptId)) {
+      return res.status(400).json({ msg: "Invalid acceptId format" });
     }
 
     const updatedRequest = await RequestModel.findByIdAndUpdate(
@@ -17,7 +23,7 @@ const acceptRequest = async (req, res) => {
       return res.status(404).json({ msg: "Request not found", success: false });
     }
 
-    res.status(204).json({ msg: "Status updated successfully", success: true });
+    res.status(200).json({ msg: "Status updated successfully", success: true });
   } catch (err) {
     res.status(500).json({
       msg: "Internal server error",
